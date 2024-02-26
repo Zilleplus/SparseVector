10a7bb01f4525ef3eb302caa87aa37b762fd4c95
Willem Melis <zilleplus@gmail.com>
2024-02-26 22:29:48 +0100

    Update build+test
    - Remove conan -> use fetch_content instead
    - Update catch2

--- test/equals.cpp
+++ test/equals.cpp
@@ -1,4 +1,4 @@
-#include<catch2/catch.hpp>
+#include<catch2/catch_test_macros.hpp>
 #include"basics.hpp"
 
 TEST_CASE("Check if [1]==[1]", "[Indexlist]"){
--- test/vector.cpp
+++ test/vector.cpp
@@ -1,4 +1,4 @@
-#include<catch2/catch.hpp>
+#include<catch2/catch_test_macros.hpp>
 #include<vector.hpp>
 
 TEST_CASE("Create simple vector with values, and read out it's values again", "[vector]"){
